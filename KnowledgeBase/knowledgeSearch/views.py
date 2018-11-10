51b470880e2b52a3f7807387e7675660986e5a19
unknown <edward.stables1198@gmail.com>
2018-11-10 00:26:05 +0000

    caused html file to print the whole set of articles when loaded

--- KnowledgeBase/knowledgeSearch/views.py
+++ KnowledgeBase/knowledgeSearch/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from knowledgeSearch.models import Article
 # Create your views here.
+
+
 def index(request):
-    return render(request, 'knowledgeSearch/index.html')
+
+    #a = Article(title = "this is just a test", body = "this is the body of the thing")
+    #a.save()
+    
+    article_list = Article.objects.all()
+
+    args = {"article_list" : article_list}
+
+    return render(request, 'knowledgeSearch/index.html', args)
