a566430b2dea578fca53e9f98257c777fd851005
unknown <edward.stables1198@gmail.com>
2018-11-10 11:06:09 +0000

    added 1minute decay on live messages

--- KnowledgeBase/knowledgeSearch/forms.py
+++ KnowledgeBase/knowledgeSearch/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 
 class NewQuestion(forms.Form):
-    firstName = forms.CharField(label = "First Name", max_length=30)
-    lastName = forms.CharField(label = "Last Name", max_length=30)
     question = forms.CharField(label = "Question", max_length=200)
     answer = forms.CharField(label = "Answer", max_length=500)
 
 class Search(forms.Form):
     keywords = forms.CharField(label = "searchBox", max_length=50)
+
+class Notify(forms.Form):
+    note = forms.CharField(label = "write message", max_length=50)
