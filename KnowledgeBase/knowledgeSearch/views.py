b37ed7bb37f3f08565dbd5dcd09e4bb9f3541155
unknown <edward.stables1198@gmail.com>
2018-11-10 02:33:28 +0000

    integrated contact table on site and contact database

--- KnowledgeBase/knowledgeSearch/views.py
+++ KnowledgeBase/knowledgeSearch/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from knowledgeSearch.models import Article
+from knowledgeSearch.models import Article, Contact
 # Create your views here.
 
 
 def index(request):
     article_list = Article.objects.all()
-
-    args = {"article_list" : article_list}
+    contact_list = Contact.objects.all()
+    args = {
+        "article_list" : article_list,
+        "contact_list" : contact_list
+        }
 
     return render(request, 'knowledgeSearch/index.html', args)
 
